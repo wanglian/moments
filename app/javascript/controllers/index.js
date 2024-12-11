25f1ecda4a03fb13438273fe3e1927b6bfe8e0ae
Will <wanglian1024@gmail.com>
2024-12-11 00:07:06 -0500

    use lightbox

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,8 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
+import Lightbox from '@stimulus-components/lightbox'
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+
 eagerLoadControllersFrom("controllers", application)
+
+application.register('lightbox', Lightbox)
--- config/importmap.rb
+++ config/importmap.rb
@@ -2,9 +2,11 @@
 
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "local-time" # @3.0.2
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
+pin "@stimulus-components/lightbox", to: "@stimulus-components--lightbox.js" # @4.0.0
+pin "lightgallery" # @2.8.2
