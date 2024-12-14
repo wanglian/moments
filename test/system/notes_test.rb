12efbb56604d07f5f5377a1e9e99c966bb7bbd45
Will <wanglian1024@gmail.com>
2024-12-13 20:45:57 -0500

    update system tests

--- test/system/notes_test.rb
+++ test/system/notes_test.rb
@@ -3,39 +3,42 @@ require "application_system_test_case"
 class NotesTest < ApplicationSystemTestCase
   setup do
     @note = notes(:one)
+    # not working??
+    visit new_session_url
+    fill_in "email_address", with: users(:will).email_address
+    fill_in "password", with: "password"
+    click_on "Sign in"
   end
 
   test "visiting the index" do
     visit notes_url
-    assert_selector "h1", text: "Notes"
+    assert_selector "div.notes"
   end
 
   test "should create note" do
     visit notes_url
-    click_on "New note"
+    click_on "Create moment"
 
     fill_in "Content", with: @note.content
-    click_on "Create Note"
+    click_on "Save"
 
-    assert_text "Note was successfully created"
-    click_on "Back"
+    assert_text "Moment was successfully created"
   end
 
   test "should update Note" do
-    visit note_url(@note)
-    click_on "Edit this note", match: :first
+    visit notes_url(anchor: dom_id(@note))
+    click_on "Edit", match: :first
 
     fill_in "Content", with: @note.content
-    click_on "Update Note"
+    click_on "Save"
 
-    assert_text "Note was successfully updated"
-    click_on "Back"
+    assert_text "Moment was successfully updated"
   end
 
   test "should destroy Note" do
-    visit note_url(@note)
-    click_on "Destroy this note", match: :first
+    visit notes_url(anchor: dom_id(@note))
+    click_on "Delete", match: :first
 
-    assert_text "Note was successfully destroyed"
+    assert_text "Moment was successfully destroyed"
   end
 end
